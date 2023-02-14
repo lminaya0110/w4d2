bf5db481dd20b3796c90ea9330735217fcde203c
lminaya0110 <100318087+lminaya0110@users.noreply.github.com>
2023-02-14 09:31:44 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,82 @@
+// I. Variables & Datatypes 
+// A. Q + A
+// B. Strings
+
+// let firstVariable = 'Hello World';
+// console.log(firstVariable);
+
+// firstVariable = 2023;
+// console.log(firstVariable);
+
+// let secondVariable = firstVariable;
+// console.log(secondVariable);
+
+// secondVariable = 'Howdy';
+// console.log(secondVariable);
+
+// console.log(firstVariable);
+
+// const yourName = 'Tina Belcher';
+
+// console.log(`Hello, my name is ${yourName}`);
+// // or this 
+// console.log(`Hello, my name is ` + yourName);
+
+// B. Strings
+// C. Booleans
+// D. The Farm
+// E. Driver's Ed
+//_____________________________________________________________
+
+
+
+// II. Loops
+// A. The Basics
+// B. Get even
+// C. Give me Five
+// D. Savings account
+//_____________________________________________________________
+
+
+
+
+// III. Arrays & Control Flow
+// A. Talk about it:
+// B. Easy Does It
+// C. Accessing elements
+// D. Change values
+// E. Mix It Up
+// F. Biggie Smalls
+// G. Monkey in the Middle
+// H. What's in Your Closet?
+//_____________________________________________________________
+
+
+
+
+// IV. Functions
+// A.
+// B. 
+// C. 
+// D.
+// E.
+// F. 
+// G.
+// H. 
+//_____________________________________________________________
+
+
+
+// V. Objects
+// A.
+// B. 
+// C. 
+// D.
+// E.
+// F. 
+// G.
+//_____________________________________________________________
+
+// Extra
+
+
